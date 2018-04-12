5c8a60a62f3ab3cb502046f71a836b3768109fef
simms21 <simms21@gmail.com>
2018-04-12 12:40:22 -0400

    mods

--- flask_sentinel/models.py
+++ flask_sentinel/models.py
@@ -48,7 +48,7 @@ class User(BaseModel):
 class Profile(BaseModel):
     """ User which will be querying resources from the API.
     """
-    def __init__(self, id=None, user,first_name=None, last_name=None, email=None):
+    def __init__(self, id=None, user=None ,first_name=None, last_name=None, email=None):
         super(User, self).__init__(id)
         self._first_name = first_name
         self._last_name = last_name
